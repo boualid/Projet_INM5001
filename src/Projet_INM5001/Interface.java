462f8aaea38e29c4788531e7e5d7998e9a6b43fa
Léopold Quenum <leopold.quenum@gmail.com>
2017-10-13 20:52:55 -0400

    Autre commit 13 oct 2017

--- src/Projet_INM5001/Interface.java
+++ src/Projet_INM5001/Interface.java
@@ -248,6 +248,12 @@ public class Interface extends javax.swing.JFrame {
         jLabel18.setForeground(new java.awt.Color(0, 173, 82));
         jLabel18.setText("Valeur maximale de la maison :");
 
+        jTextField7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField7ActionPerformed(evt);
+            }
+        });
+
         ButEffacer.setBackground(new java.awt.Color(11, 109, 11));
         ButEffacer.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         ButEffacer.setForeground(new java.awt.Color(255, 255, 255));
@@ -545,40 +551,10 @@ public class Interface extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jTMois3ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+    private void jTextField7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField7ActionPerformed
+       
+    }//GEN-LAST:event_jTextField7ActionPerformed
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Interface().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton ButCalculer;
--- src/Projet_INM5001/Principale.java
+++ src/Projet_INM5001/Principale.java
@@ -16,28 +16,40 @@ import java.util.logging.Logger;
  */
 public class Principale {
 
-    /**
+        /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // Test methode requetSelect() BD
-        String reqSelect = "select * from Amortissement";
-        BaseDeDonnees.requeteSelect(reqSelect);
-        /*try {
-            while (resSet.next()) {
-                int idAmort = resSet.getInt("idAmortissement");
-                int dureeAmort = resSet.getInt("dureeAmortissement");
-                System.out.println(idAmort + "\t" + dureeAmort);
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
             }
-            // Fin test methode requetSelect()
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }*/
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Interface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
 
-        
-        // Test calcul pret hypothécaire (pretHypClient)
-        
-        // Fin test calcul pret hypothécaire (pretHypClient)
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Interface().setVisible(true);
+            }
+        });
     }
+
     
 }
